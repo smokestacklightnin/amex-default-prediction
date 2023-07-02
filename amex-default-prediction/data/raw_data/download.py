35c81f2e8d9fa65114958e5ce891bf49437a1c01
William Black <wblack1845@gmail.com>
2023-07-02 01:11:38 -0700

    Clean up code

--- amex-default-prediction/data/raw_data/download.py
+++ amex-default-prediction/data/raw_data/download.py
@@ -21,12 +21,11 @@ _default_checksum_path = files(
 
 
 def _hash_file(filename, block_size=65536):
+    filename = pathlib.Path(filename)
     h = hashlib.sha512()
-
+    logging.debug("Calculating hash of " + str(filename))
     with open(filename, "r") as file:
-        chunk = 0
-        while chunk != "":
-            chunk = file.read(block_size)
+        while (chunk := file.read(block_size)) != "":
             h.update(chunk.encode("utf-8"))
 
     return h.hexdigest()
@@ -37,16 +36,21 @@ def _verify_raw_data(path=None, checksum_path=None):
         path = _default_output_path
     if checksum_path is None:
         checksum_path = _default_checksum_path
+    path, checksum_path = pathlib.Path(path), pathlib.Path(checksum_path)
 
-    checksums = dict()
-    with open(checksum_path, "r") as checksum_file:
-        for checksum_line in checksum_file:
-            ch, fn = checksum_line.split()
-            checksums.update({fn: ch})
+    try:
+        checksums = dict()
+        with open(checksum_path, "r") as checksum_file:
+            for checksum_line in checksum_file:
+                ch, fn = checksum_line.split()
+                checksums.update({fn: ch})
 
-    for filename, checksum in checksums.items():
-        if _hash_file(path.joinpath(filename)) != checksum:
-            return False
+        for filename, checksum in checksums.items():
+            if _hash_file(path.joinpath(filename)) != checksum:
+                return False
+    except FileNotFoundError:
+        logging.warning("The raw data files were not found")
+        return False
 
     return True
 
@@ -63,14 +67,15 @@ def from_kaggle(path=None, checksum_path=None):
             "There was something wrong with the raw data files. Downlading and extracting again."
         )
         with tempfile.TemporaryDirectory() as tmpdirname:
+            tmpdirpath = pathlib.Path(tmpdirname)
             kaggle.api.competition_download_files(
                 _competition_name,
-                path=tmpdirname,
+                path=tmpdirpath,
                 quiet=False,
             )
-            print(type(tmpdirname), tmpdirname)
+
             with zipfile.ZipFile(
-                pathlib.Path(tmpdirname).joinpath(_competition_name + ".zip")
+                tmpdirpath.joinpath(_competition_name + ".zip")
             ) as data_zip:
                 data_zip.extractall(path=path)
         logging.info("Successfully downloaded and extracted raw data files.")
