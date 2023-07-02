628ab5e1c289c750cb8937c1a9ab57d85f83a4bd
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2023-07-02 00:11:13 -0700

    Download and extract missing or corrupted data

--- amex-default-prediction/data/raw_data/download.py
+++ amex-default-prediction/data/raw_data/download.py
@@ -4,13 +4,20 @@ import hashlib
 from importlib_resources import files
 import logging
 import kaggle
+import pathlib
 
-logging.basicConfig(level=logging.INFO)
-
-_default_output_path = files("amex-default-prediction.data.raw_data")
-_default_checksum_path = files("amex-default-prediction.data.raw_data").joinpath(
-    "sha512sums.txt"
+logging.basicConfig(
+    level=logging.DEBUG,
 )
+
+
+_competition_name = "amex-default-prediction"
+_default_output_path = files(
+    _competition_name + ".data.raw_data",
+)
+_default_checksum_path = files(
+    _competition_name + ".data.raw_data",
+).joinpath("sha512sums.txt")
 
 
 def _hash_file(filename, block_size=65536):
@@ -49,6 +56,7 @@ def from_kaggle(path=None, checksum_path=None):
         path = _default_output_path
     if checksum_path is None:
         checksum_path = _default_checksum_path
+    path, checksum_path = pathlib.Path(path), pathlib.Path(checksum_path)
 
     if not _verify_raw_data(path, checksum_path):
         logging.warning(
@@ -56,8 +64,16 @@ def from_kaggle(path=None, checksum_path=None):
         )
         with tempfile.TemporaryDirectory() as tmpdirname:
             kaggle.api.competition_download_files(
-                "amex-default-prediction", path=tmpdirname
+                _competition_name,
+                path=tmpdirname,
+                quiet=False,
             )
+            print(type(tmpdirname), tmpdirname)
+            with zipfile.ZipFile(
+                pathlib.Path(tmpdirname).joinpath(_competition_name + ".zip")
+            ) as data_zip:
+                data_zip.extractall(path=path)
+        logging.info("Successfully downloaded and extracted raw data files.")
     else:
         logging.info("All raw data files already downloaded")
 
