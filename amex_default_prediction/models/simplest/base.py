6b82f8b7dd814b3b2488fb9ed990258baf67a90c
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2023-08-31 21:17:53 -0700

    Add single densely connected layer with relu

--- amex_default_prediction/models/simplest/base.py
+++ amex_default_prediction/models/simplest/base.py
@@ -2,3 +2,15 @@ import jax
 from jax import numpy as jnp
 import flax
 from flax import linen as nn
+import optax
+
+
+class Simplest(nn.Module):
+    def setup(self, nFeatures):
+        self.dense_1 = nn.Dense(nFeatures)
+        self.relu_1 = nn.relu
+
+    def __call__(self, x):
+        x = self.dense_1(x)
+        x = self.relu_1(x)
+        return x
