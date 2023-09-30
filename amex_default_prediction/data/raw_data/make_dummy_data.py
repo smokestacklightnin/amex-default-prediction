e201089a8a9fe8d9b8d25d768a83321b459537c1
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2023-09-29 21:44:11 -0700

    Add function to get train data subset from train label subset

--- amex_default_prediction/data/raw_data/make_dummy_data.py
+++ amex_default_prediction/data/raw_data/make_dummy_data.py
@@ -19,6 +19,10 @@ train_labels_full = (
     .load("./train_labels.csv")
 )
 
+train_data_full = (
+    spark.read.format("csv").option("header", True).load("./train_data.csv")
+)
+
 
 def make_train_labels_subset(fraction=0.1):
     return train_labels_full.sample(
@@ -27,5 +31,14 @@ def make_train_labels_subset(fraction=0.1):
     )
 
 
+def make_train_data_subset_from_train_labels(labels):
+    return train_data_full.join(
+        labels_subset,
+        "customer_ID",
+        "right",
+    )
+
+
 if __name__ == "__main__":
     labels_subset = make_train_labels_subset(0.02)
+    data_subset = make_train_data_subset_from_train_labels(labels_subset)
