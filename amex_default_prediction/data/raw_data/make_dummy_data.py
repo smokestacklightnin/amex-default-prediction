2e09e385107f5a46525b71ece96f56fe3736845b
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2023-09-29 23:21:19 -0700

    Add data subset writer

--- amex_default_prediction/data/raw_data/make_dummy_data.py
+++ amex_default_prediction/data/raw_data/make_dummy_data.py
@@ -5,6 +5,11 @@ from pyspark.sql.types import (
     StringType,
     IntegerType,
 )
+from importlib.resources import files as get_module_path
+
+default_data_path = get_module_path("amex_default_prediction.data.raw_data")
+default_train_data_filename = "train_data_subset.csv"
+default_test_data_filename = "test_data_subset.csv"
 
 spark = SparkSession.builder.master("local[8]").getOrCreate()
 
@@ -14,7 +19,7 @@ train_labels_full = (
     .schema(
         StructType()
         .add("customer_ID", StringType(), True)
-        .add("defaulted", IntegerType(), True)
+        .add("target", IntegerType(), True)
     )
     .load("./train_labels.csv")
 )
@@ -33,12 +38,22 @@ def make_train_labels_subset(fraction=0.1):
 
 def make_train_data_subset_from_train_labels(labels):
     return train_data_full.join(
-        labels_subset,
+        labels,
         "customer_ID",
         "right",
     )
 
 
+def write_data_subset(df, filename, filepath=default_data_path):
+    df.write.options(
+        header=True,
+    ).mode(
+        "overwrite"
+    ).csv(str(filepath.joinpath(filename)), compression="bzip2")
+
+
 if __name__ == "__main__":
-    labels_subset = make_train_labels_subset(0.02)
-    data_subset = make_train_data_subset_from_train_labels(labels_subset)
+    train_labels_subset = make_train_labels_subset(0.04)
+    train_data_subset = make_train_data_subset_from_train_labels(train_labels_subset)
+    write_data_subset(train_labels_subset, "train_labels_subset")
+    write_data_subset(train_data_subset, "train_data_subset")
